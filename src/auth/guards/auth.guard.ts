a0e696475d12fc131071d72292e703a042ab6ca7
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-09-11 20:30:29 -0600

    validate JWT and get user pt2

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,12 +1,14 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from '../interfaces/jwt-payload';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private authService: AuthService,
   ) {}
 
   async canActivate( context: ExecutionContext ): Promise<boolean> {
@@ -21,14 +23,18 @@ export class AuthGuard implements CanActivate {
       const payload = await this.jwtService.verifyAsync<JwtPayload>(
         token, { secret: process.env.JWT_SEED }
       );
-      console.log({ payload });
+
+      const user = await this.authService.findUserById( payload.id );
+      if( !user ) throw new UnauthorizedException('User does not exists!');
+      if( !user.isActive ) throw new UnauthorizedException('User is not active.');
   
-      request['user'] = payload;
+      request['user'] = user;
+
     } catch(error) {
       throw new UnauthorizedException();
     }
     
-    return Promise.resolve(true);
+    return true;
   }
 
   private extractTokenFromHeader( request: Request ): string | undefined {
