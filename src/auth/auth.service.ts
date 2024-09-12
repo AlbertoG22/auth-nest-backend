d9d5bbb867910da7f39667d6cd1170bda6a30e48
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-09-11 19:22:23 -0600

    generate JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,8 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import * as bcryptjs from 'bcryptjs';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload';
 
 @Injectable()
 export class AuthService {
@@ -13,6 +15,7 @@ export class AuthService {
   constructor(
     @InjectModel(User.name)
     private userModel: Model<User>,
+    private jwtService: JwtService,
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
@@ -56,7 +59,7 @@ export class AuthService {
 
     return {
       user: rest,
-      token: 'ABC-123'
+      token: this.getJwtToken({ id: user.id }),
     };
 
     /*
@@ -80,5 +83,10 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  getJwtToken( payload: JwtPayload ) {
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 }
